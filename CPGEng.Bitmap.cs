4cc4cf8c252f3bf0b1c7d278d735dec49e6d7291
crispycat <30805430+crispycat0@users.noreply.github.com>
2019-10-07 20:29:19 -0400

    Upload v2.0pre01

--- CPGEng.Bitmap.cs
+++ CPGEng.Bitmap.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Crispycat PixelGraphic Engine
  * CPGEng.Bitmap.cs; Bitmap tools
- * (C) 2019 crispycat; https://github.com/crispycat0/CPGEng
- * 2019/09/29
+ * (C) 2019 crispycat; https://github.com/crispycat0/CPGEng/LICENSE
+ * 2019/10/07
 */
 
 using System;
@@ -10,15 +10,33 @@ using System.Windows.Media.Imaging;
 
 namespace CPGEng {
 	public class BitmapData {
-		public readonly int Width, Height, Channels, Stride;
+		public readonly int Width, Height, Channels = 4, Stride;
 		public Buffer buffer;
 
-		public BitmapData(int w, int h, Buffer b, int c = 4, int s = 0) {
+		public BitmapData(int w, int h, Buffer b, int s = 0) {
 			Width = w;
 			Height = h;
 			buffer = b;
-			Channels = c;
-			Stride = (s == 0) ? w * c + (w % 8) : s;
+			Stride = (s == 0) ? w * Channels + (w % Channels) : s;
+		}
+
+		public uint PixelLocationInBuffer(Pixel p) {
+			return (uint)(p.X * Channels + p.Y * Stride);
+		}
+
+		public ColorInt Get(Pixel p) {
+			return new ColorInt(
+				buffer.Get(PixelLocationInBuffer(p)),
+				buffer.Get(PixelLocationInBuffer(p) + 1),
+				buffer.Get(PixelLocationInBuffer(p) + 2)
+			);
+		}
+
+		public void Draw(Pixel p, ColorInt c) {
+			buffer.Set(PixelLocationInBuffer(p), (byte)c.Blue);
+			buffer.Set(PixelLocationInBuffer(p) + 1, (byte)c.Green);
+			buffer.Set(PixelLocationInBuffer(p) + 2, (byte)c.Red);
+			buffer.Set(PixelLocationInBuffer(p) + 3, 255);
 		}
 
 		public BitmapSource ToBitmapSource() {
@@ -27,7 +45,7 @@ namespace CPGEng {
 	}
 
 	public static class Bitmap {
-		public static BitmapData Import(string path, int w = 256, int h = 256, int c = 4) {
+		public static BitmapData Import(string path, int w = 256, int h = 256) {
 			BitmapImage img = new BitmapImage();
 			img.BeginInit();
 			img.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
@@ -35,18 +53,61 @@ namespace CPGEng {
 			img.DecodePixelHeight = h;
 			img.EndInit();
 
-			byte[] data = new byte[w * h * c];
-			img.CopyPixels(data, w * c, 0);
+			byte[] data = new byte[w * h * 4];
+			img.CopyPixels(data, w * 4, 0);
 
-			return new BitmapData(w, h, new Buffer(data), c, w * c);
+			return new BitmapData(w, h, new Buffer(data), w * 4);
 		}
 
 		public static BitmapData FromView(View v) {
-			return new BitmapData((int)v.Width, (int)v.Height, v.buffer, (int)v.Channels, (int)v.Stride);
+			return new BitmapData((int)v.Width, (int)v.Height, v.buffer, (int)v.Stride);
 		}
 
-		public static BitmapData Resize(BitmapData b, int w, int h) {
-			return new BitmapData(0, 0, null);
+		public static BitmapData Resize(BitmapData b, int w, int h, ScalingMode m = 0) {
+			BitmapData nb = new BitmapData(w, h, new Buffer((uint)(w * h * 4)), w * 4);
+
+			if (m == ScalingMode.Bilinear) {
+				if (w == 0 || h == 0) return b;
+				double rx = w / b.Width;
+				double ry = h / b.Height;
+
+				for (int x = 0; x < w; x++) {
+					for (int y = 0; y < h; y++) {
+						double gx = (double)x / w * (b.Width - 1);
+						double gy = (double)y / h * (b.Height - 1);
+						int ix = (int)gx;
+						int iy = (int)gy;
+						double dx = gx - ix;
+						double dy = gy - iy;
+
+						ColorInt c00 = b.Get(new Pixel(ix, iy));
+						ColorInt c10 = b.Get(new Pixel(ix + 1, iy));
+						ColorInt c01 = b.Get(new Pixel(ix, iy + 1));
+						ColorInt c11 = b.Get(new Pixel(ix + 1, iy + 1));
+
+						int ob = (int)Interpolation.Blerp(c00.Blue, c10.Blue, c01.Blue, c11.Blue, dx, dy);
+						int og = (int)Interpolation.Blerp(c00.Green, c10.Green, c01.Green, c11.Green, dx, dy);
+						int or = (int)Interpolation.Blerp(c00.Red, c10.Red, c01.Red, c11.Red, dx, dy);
+
+						nb.Draw(new Pixel(x, y), new ColorInt(ob, og, or));
+					}
+				}
+			} else {
+				double rx = (double)b.Width / w;
+				double ry = (double)b.Height / h;
+				double px, py;
+
+				for (int i = 0; i < h; i++) {
+					for (int j = 0; j < w; j++) {
+						px = Math.Floor(j * rx);
+						py = Math.Floor(i * ry);
+
+						nb.Draw(new Pixel(j, i), b.Get(new Pixel((int)px, (int)py)));
+					}
+				}
+			}
+
+			return nb;
 		}
 	}
 }
---
+++ CPGEng.Interpolation.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Crispycat PixelGraphic Engine
+ * CPGEng.Interpolation.cs; Interpolation functions
+ * (C) 2019 crispycat; https://github.com/crispycat0/CPGEng/LICENSE
+ * 2019/10/07
+*/
+
+namespace CPGEng {
+	public static class Interpolation {
+		public static double Lerp(double s, double e, double t) {
+			return s + (e - s) * t;
+		}
+
+		public static double Blerp(double c00, double c10, double c01, double c11, double tx, double ty) {
+			return Lerp(Lerp(c00, c10, tx), Lerp(c01, c11, tx), ty);
+		}
+	}
+}
